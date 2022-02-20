0cf8c97740dc781e7a6d6bc9287f19c846db7bb9
antonioLucianoNeves <a_luciano10@hotmail.com>
2022-02-20 12:28:46 -0300

    Array com ojeto

---
+++ ArrayObjeto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price};
+            }
+            double media = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                media =+ vect[i].Price;
+            }
+
+            double soma = media / n;
+            Console.WriteLine("Valor médio :" + soma.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
